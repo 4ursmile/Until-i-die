116528ee7f55b20c626f6b6aefad359549ebaf65
UIT-20521864 <75603259+tata1207@users.noreply.github.com>
2022-05-26 21:55:22 +0700

    Add files via upload

---
+++ Assets/Sound/Music/FootStepSound.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepSound : MonoBehaviour
+{
+    [SerializeField]AudioSource audioSource;
+    [SerializeField] AudioClip audioclip;
+    void Start()
+    {
+    }
+    public void playFootStep()
+    {
+        audioSource.PlayOneShot(audioclip);
+    }
+}
